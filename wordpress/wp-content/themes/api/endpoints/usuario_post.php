1ab1fc0e19ddbaf7647edeeb5936b5f9f724e550
Pw <pablo_weslly@hotmail.com>
2019-11-12 18:55:45 -0300

    feat: Estrutura inicial - Wordpress com docker e funcionando API com get - apesar que no avaçar da vide aula vai ser um post - para acessar localhost:8091:/wp-json/api/usuarios

---
+++ wordpress/wp-content/themes/api/endpoints/usuario_post.php
@@ -0,0 +1,38 @@
+<?php
+function api_usuario_post($request)
+{
+  $response = array(
+    'nome' => 'andre',
+    'email' => 'pablo@gmail.com'
+  );
+
+
+  /* 
+     Essa função vai retorna isso, quer dizer dê uma reposta do
+     tipo api do wordpress 
+  */
+  return rest_ensure_response($response);
+}
+
+/*
+  Agora preciso só registrar minha API na rest do wordpress
+*/
+function registrar_api_usuario_post()
+{
+  /* 
+    Esse função vai chamar uma função real 
+    do wordpress que registra API
+  */
+  register_rest_route('api', '/usuario', array(
+    array(
+      # Aqui no metodo eu digo, se é post, put, delete entre outros.
+      #'methods' => WP_REST_Server::CREATABLE,
+      'methods' => 'GET',
+      # Aqui retorno, nesse caso vai executar função lá em cima
+      'callback' => 'api_usuario_post',
+    ),
+  ));
+}
+
+#tenho que adicionar função no wordpress
+add_action('rest_api_init', 'registrar_api_usuario_post');
