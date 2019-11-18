75913281e3cdf97eb1604ca80f7c9138b93d1753
pablo <pablo_weslly@hotmail.com>
2019-11-18 14:40:28 -0300

    feat: Aula 7 - Função get usuario e exibindo as informacoes que quero via tokenn  04:50 - Funcionando o plugin

--- wordpress/wp-content/themes/api/endpoints/usuario_get.php
+++ wordpress/wp-content/themes/api/endpoints/usuario_get.php
@@ -1,11 +1,38 @@
 <?php
 function api_usuario_get($request)
 {
-
-  # Vamos pegar dados atual do usuário logado
-
   $user = wp_get_current_user();
-  return rest_ensure_response($user);
+  $user_id = $user->ID;
+
+  if ($user_id > 0) {
+    $user_meta = get_user_meta($user_id);
+    /*
+     Esse user_meta é diferente do user
+     no user estou pegando o ID pelo GET
+     e no $user_id  estou atribuindo o ID peguei pelo
+     get na variaǘel $user.  No $user_meta  uso função
+     do worpdress para pegar informações no banco de dados
+     referente ao usuário do ID capturado pelo GET.
+    */
+
+
+    $response = array(
+      "id" => $user->user_login,
+      "nome" => $user->display_name,
+      "email" => $user->user_email,
+      "cep" => $user_meta['cep'][0],
+      "numero" => $user_meta['numero'][0],
+      "rua" => $user_meta['rua'][0],
+      "bairro" => $user_meta['bairro'][0],
+      "cidade" => $user_meta['cidade'][0],
+      "estado" => $user_meta['estado'][0]
+      // Coloquei 0 porque é primeiro item quero da array
+
+    );
+  } else {
+    $response = new WP_Error('permissao', 'Usuario não tem permisão', array('status' => 401));
+  }
+  return rest_ensure_response($response);
 }
 
 function registrar_api_usuario_get()
